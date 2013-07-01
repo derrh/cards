98159e1150b5c3d907e7caaba9c50ff99b641d0b
Derrick J. Hathaway <derrick@instructure.com>
2013-07-01 12:59:20 -0600

    Initial Commit

---
+++ cards/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cards
+//
+//  Created by derrick on 7/1/13.
+//  Copyright (c) 2013 derrh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
